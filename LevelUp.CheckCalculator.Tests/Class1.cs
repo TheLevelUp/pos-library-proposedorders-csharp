09231b8b5f3db06aec4638f3b7ba4789d8678aea
Forest Balk <forest.balk@scvngr.com>
2017-04-11 19:09:59 -0400

    Added sample unit testing project

---
+++ LevelUp.CheckCalculator.Tests/Class1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LevelUp.CheckCalculator.Tests
+{
+    [TestClass]
+    public class Class1
+    {
+        [TestMethod]
+        public void TestTemplate()
+        {
+
+        }
+    }
+}
