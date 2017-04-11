8c73e9d1073ed8faba4f5fdf407e9d4925a8d5d9
Forest Balk <forest.balk@scvngr.com>
2017-04-11 19:54:19 -0400

    Brought over discount calculator logic

---
+++ LevelUp.CheckCalculator.Tests/CalculatorTests.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+
+namespace LevelUp.CheckCalculator.Tests
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        [TestMethod]
+        public void DiscountToApply_MiscellaneousTests()
+        {
+            /* 4 States
+             * Compare 'Payment' to 'Due W/Tax' where 'Due W/Tax' is less than, equal, greater than, and 0
+             * 
+             * 4 States
+             * Compare 'MFC' to 'Payment' where 'MFC' is less than, equal, greater than, and 0
+             * 
+             * 5 States
+             * Compare 'Exempt' to 'MFC' or 'Due W/Tax' where 'Exempt' is less than, equal, greater than, and 0
+             * 
+             * Results in 4 * 4 * 5 test cases.
+             */
+            decimal[,] values = {
+                //MFC, Payment, Due W/Tax, Tax, Exempt, Expected Discount
+                {5m, 10m, 10m, 1m, 3m, 5m},
+                {5m, 10m, 10m, 1m, 5m, 4m},
+                {5m, 10m, 10m, 1m, 10m, 0m},
+                {5m, 10m, 10m, 1m, 20m, 0m},
+                {5m, 10m, 10m, 1m, 0m, 5m},
+                {10m, 10m, 10m, 1m, 3m, 6m},
+                {10m, 10m, 10m, 1m, 5m, 4m},
+                {10m, 10m, 10m, 1m, 10m, 0m},
+                {10m, 10m, 10m, 1m, 20m, 0m},
+                {10m, 10m, 10m, 1m, 0m, 9m},
+                {20m, 10m, 10m, 1m, 3m, 6m},
+                {20m, 10m, 10m, 1m, 5m, 4m},
+                {20m, 10m, 10m, 1m, 10m, 0m},
+                {20m, 10m, 10m, 1m, 20m, 0m},
+                {20m, 10m, 10m, 1m, 0m, 9m},
+                {0m, 10m, 10m, 1m, 3m, 0m},
+                {0m, 10m, 10m, 1m, 5m, 0m},
+                {0m, 10m, 10m, 1m, 10m, 0m},
+                {0m, 10m, 10m, 1m, 20m, 0m},
+                {0m, 10m, 10m, 1m, 0m, 0m},
+                {5m, 10m, 11m, 1m, 3m, 5m},
+                {5m, 10m, 11m, 1m, 5m, 5m},
+                {5m, 10m, 11m, 1m, 10m, 0m},
+                {5m, 10m, 11m, 1m, 20m, 0m},
+                {5m, 10m, 11m, 1m, 0m, 5m},
+                {10m, 10m, 11m, 1m, 3m, 7m},
+                {10m, 10m, 11m, 1m, 5m, 5m},
+                {10m, 10m, 11m, 1m, 10m, 0m},
+                {10m, 10m, 11m, 1m, 20m, 0m},
+                {10m, 10m, 11m, 1m, 0m, 10m},
+                {20m, 10m, 11m, 1m, 3m, 7m},
+                {20m, 10m, 11m, 1m, 5m, 5m},
+                {20m, 10m, 11m, 1m, 10m, 0m},
+                {20m, 10m, 11m, 1m, 20m, 0m},
+                {20m, 10m, 11m, 1m, 0m, 10m},
+                {0m, 10m, 11m, 1m, 3m, 0m},
+                {0m, 10m, 11m, 1m, 5m, 0m},
+                {0m, 10m, 11m, 1m, 10m, 0m},
+                {0m, 10m, 11m, 1m, 20m, 0m},
+                {0m, 10m, 11m, 1m, 0m, 0m},
+                {5m, 10m, 12m, 1m, 3m, 5m},
+                {5m, 10m, 12m, 1m, 5m, 5m},
+                {5m, 10m, 12m, 1m, 10m, 1m},
+                {5m, 10m, 12m, 1m, 20m, 0m},
+                {5m, 10m, 12m, 1m, 0m, 5m},
+                {10m, 10m, 12m, 1m, 3m, 8m},
+                {10m, 10m, 12m, 1m, 5m, 6m},
+                {10m, 10m, 12m, 1m, 10m, 1m},
+                {10m, 10m, 12m, 1m, 20m, 0m},
+                {10m, 10m, 12m, 1m, 0m, 10m},
+                {20m, 10m, 12m, 1m, 3m, 8m},
+                {20m, 10m, 12m, 1m, 5m, 6m},
+                {20m, 10m, 12m, 1m, 10m, 1m},
+                {20m, 10m, 12m, 1m, 20m, 0m},
+                {20m, 10m, 12m, 1m, 0m, 10m},
+                {0m, 10m, 12m, 1m, 3m, 0m},
+                {0m, 10m, 12m, 1m, 5m, 0m},
+                {0m, 10m, 12m, 1m, 10m, 0m},
+                {0m, 10m, 12m, 1m, 20m, 0m},
+                {0m, 10m, 12m, 1m, 0m, 0m},
+                {5m, 10m, 0m, 0m, 3m, 0m},
+                {5m, 10m, 0m, 0m, 5m, 0m},
+                {5m, 10m, 0m, 0m, 10m, 0m},
+                {5m, 10m, 0m, 0m, 20m, 0m},
+                {5m, 10m, 0m, 0m, 0m, 0m},
+                {10m, 10m, 0m, 0m, 3m, 0m},
+                {10m, 10m, 0m, 0m, 5m, 0m},
+                {10m, 10m, 0m, 0m, 10m, 0m},
+                {10m, 10m, 0m, 0m, 20m, 0m},
+                {10m, 10m, 0m, 0m, 0m, 0m},
+                {20m, 10m, 0m, 0m, 3m, 0m},
+                {20m, 10m, 0m, 0m, 5m, 0m},
+                {20m, 10m, 0m, 0m, 10m, 0m},
+                {20m, 10m, 0m, 0m, 20m, 0m},
+                {20m, 10m, 0m, 0m, 0m, 0m},
+                {0m, 10m, 0m, 0m, 3m, 0m},
+                {0m, 10m, 0m, 0m, 5m, 0m},
+                {0m, 10m, 0m, 0m, 10m, 0m},
+                {0m, 10m, 0m, 0m, 20m, 0m},
+                {0m, 10m, 0m, 0m, 0m, 0m}
+            };
+
+            RunTestArray(values);
+        }
+
+        private void RunTestArray(decimal[,] values)
+        {
+            for (int i = 0; i < values.GetLength(0); i++)
+            {
+                Calculator.CalculateDiscountToApply(values[i, 0],
+                    values[i, 1],
+                    values[i, 2],
+                    values[i, 3],
+                    values[i, 4]).Should().Be(values[i, 5], "row={0}", i);
+            }
+        }
+    }
+}
---
+++ LevelUp.CheckCalculator/Calculator.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace LevelUp.CheckCalculator
+{
+    public static class Calculator
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="checkTotalAmount">The full total amount of the check (including tax), in cents.</param>
+        /// <param name="taxAmount">The amount of tax currently due on the check, in cents.</param>
+        /// <param name="spendAmount">The amount the customer is requesting to pay, in cents.</param>
+        /// <param name="discountAvailableAmount">The amount of discount available, in cents.</param>
+        /// <param name="exemptionAmount">The total amount of exempted items on the check, in cents.</param>
+        /// <returns></returns>
+        //public static int CalculateDiscountToApply(int checkTotalAmount,
+        //                                           int taxAmount,
+        //                                           int spendAmount,
+        //                                           int discountAvailableAmount,
+        //                                           int exemptionAmount = 0)
+        //{
+        //    int amountDueLessTax = checkTotalAmount - taxAmount;
+
+        //    int paymentRequested = Math.Max(0, Math.Min(spendAmount, amountDueLessTax));
+
+        //    if (paymentRequested < amountDueLessTax)
+        //    {
+        //        int exemptionAmountToIgnore = amountDueLessTax - paymentRequested;
+
+        //        // if attempting a partial payment, reduce the exemption considered against the payment amount
+        //        // by the difference between that payment amount and the actual total due
+        //        exemptionAmount = Math.Max(0, exemptionAmount - exemptionAmountToIgnore);
+        //    }
+
+        //    return Math.Min(discountAvailableAmount, Math.Max(0, spendAmount - exemptionAmount));
+        //}
+
+
+        /// <summary>
+        /// Calculates the LevelUp discount to apply based on the arguments passed
+        /// </summary>
+        /// <param name="merchantFundedCreditAvailableInDollars">The merchant funded credit amount available in dollars returned from the LevelUp API</param>
+        /// <param name="paymentAmountInDollars">The requested payment amount from the cashier/customer in dollars</param>
+        /// <param name="amountDueInDollars">The remaining amount due on the check including tax in dollars</param>
+        /// <param name="taxAmountDueInDollars">The tax amount due on the check in dollars</param>
+        /// <param name="exemptedItemsTotalInDollars">Total cost of all exempted items on the check in dollars</param>
+        /// <returns>The LevelUp discount to apply in dollars</returns>
+        /// <see cref="http://developer.thelevelup.com/api-reference/v14/merchant-funded-credit/"/>
+        public static decimal CalculateDiscountToApply(decimal merchantFundedCreditAvailableInDollars,
+            decimal paymentAmountInDollars,
+            decimal amountDueInDollars,
+            decimal taxAmountDueInDollars,
+            decimal exemptedItemsTotalInDollars)
+        {
+            decimal amountDueLessTax = amountDueInDollars - taxAmountDueInDollars;
+
+            decimal paymentRequested = Math.Max(0, Math.Min(paymentAmountInDollars, amountDueLessTax));
+
+            if (paymentRequested < amountDueLessTax)
+            {
+                decimal exemptionAmountToIgnore = amountDueLessTax - paymentRequested;
+
+                // if attempting a partial payment, reduce the exemption considered against the payment amount
+                // by the difference between that payment amount and the actual total due
+                exemptedItemsTotalInDollars = Math.Max(0, exemptedItemsTotalInDollars - exemptionAmountToIgnore);
+            }
+
+            return CalculateDiscountToApply(merchantFundedCreditAvailableInDollars,
+                paymentRequested,
+                exemptedItemsTotalInDollars);
+        }
+
+        /// <summary>
+        /// Calculates the LevelUp discount to apply based on the arguments passed
+        /// </summary>
+        /// <param name="merchantFundedCreditAvailableInDollars">The merchant funded credit amount available in dollars returned from the LevelUp API</param>
+        /// <param name="paymentAmountInDollars">The requested, pre-tax payment amount from the cashier/customer in dollars</param>
+        /// <param name="exemptedItemsTotalInDollars">Total cost of all exempted items on the check in dollars</param>
+        /// <returns>The LevelUp discount to apply in dollars</returns>
+        /// <see cref="http://developer.thelevelup.com/api-reference/v14/merchant-funded-credit/"/>
+        public static decimal CalculateDiscountToApply(decimal merchantFundedCreditAvailableInDollars,
+            decimal paymentAmountInDollars,
+            decimal exemptedItemsTotalInDollars)
+        {
+            return Math.Min(merchantFundedCreditAvailableInDollars,
+                Math.Max(0, paymentAmountInDollars - exemptedItemsTotalInDollars));
+        }
+    }
+}
