6e5600298a96f73e0b26f103f31fcca7c38af995
Forest Balk <forest.balk@scvngr.com>
2017-04-15 14:55:47 -0400

    Initial add of suggested implementation for third parties; tests pass

---
+++ LevelUp.Pos.ProposedOrderCalculator.Tests/CalculatorTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+using LevelUp.Pos.ProposedOrderCalculator.Tests.Data;
+
+namespace LevelUp.Pos.ProposedOrderCalculator.Tests
+{
+    public class CalculatorTests
+    {
+        [TestClass]
+        public class AlternativeCalculatorTests
+        {
+            [TestMethod]
+            public void RunTestBattery()
+            {
+                RunTestArray(TestData.TestBattery);
+            }
+
+            private void RunTestArray(int[,] values)
+            {
+                for (int i = 0; i < values.GetLength(0); i++)
+                {
+                    int totalOutstandingAmount = values[i, 0];
+                    int totalOutstandintotalTaxAmountgAmount = values[i, 1];
+                    int totalExemptionAmount = values[i, 2];
+                    int spendAmount = values[i, 3];
+
+                    int expectedSpendAmount = values[i, 4];
+                    int expectedTaxAmount = values[i, 5];
+                    int expectedExemptionAmount = values[i, 6];
+
+                    AdjustedCheckValues expectedCheckValues =
+                        new AdjustedCheckValues(expectedSpendAmount, expectedTaxAmount, expectedExemptionAmount);
+
+                    AdjustedCheckValues actualCheckValues = Calculator.CalculateAdjustedCheckValues(
+                        totalOutstandingAmount,
+                        totalOutstandintotalTaxAmountgAmount,
+                        totalExemptionAmount,
+                        spendAmount);
+
+                    actualCheckValues.ShouldBeEquivalentTo(expectedCheckValues,
+                        "row={0}; {3}Exected:{1}; {3}Actual: {2}",
+                        i, expectedCheckValues, actualCheckValues, Environment.NewLine);
+                }
+            }
+        }
+    }
+}
---
+++ LevelUp.Pos.ProposedOrderCalculator/Calculator.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace LevelUp.Pos.ProposedOrderCalculator
+{
+    public class AdjustedCheckValues
+    {
+        public int SpendAmount { get; set; }
+        public int TaxAmount { get; set; }
+        public int ExemptionAmount { get; set; }
+
+        public AdjustedCheckValues(int spendAmount, int taxAmount, int exemptionAmount)
+        {
+            SpendAmount = spendAmount;
+            TaxAmount = taxAmount;
+            ExemptionAmount = exemptionAmount;
+        }
+
+        public override string ToString() =>
+            $"SpendAmount={SpendAmount};TaxAmount={TaxAmount};ExemptionAmount={ExemptionAmount}";
+    }
+
+    public class Calculator
+    {
+        public static AdjustedCheckValues CalculateAdjustedCheckValues(
+            int totalOutstandingAmount,
+            int totalTaxAmount,
+            int totalExemptionAmount,
+            int spendAmount
+        )
+        {
+            int adjustedSpendAmount = CalculateAdjustedSpendAmount(spendAmount, totalOutstandingAmount);
+
+            int adjustedTaxAmount =
+                CalculateAdjustedTaxAmount(totalOutstandingAmount, totalTaxAmount, adjustedSpendAmount);
+
+            int adjustedExemptionAmount =
+                CalculatedAdjustedExemptionAmount(totalOutstandingAmount, totalTaxAmount, totalExemptionAmount,
+                    adjustedSpendAmount);
+
+            return new AdjustedCheckValues(adjustedSpendAmount, adjustedTaxAmount, adjustedExemptionAmount);
+        }
+
+        private static int CalculateAdjustedSpendAmount(int totalOutstandingAmount, int spendAmount)
+        {
+            return Math.Max(0, Math.Min(spendAmount, totalOutstandingAmount));
+        }
+
+        private static int CalculateAdjustedTaxAmount(
+            int totalOutstandingAmount,
+            int totalTaxAmount,
+            int adjustedSpendAmount)
+        {
+            int adjustedTaxAmount = totalTaxAmount;
+
+            bool wasPartialPaymentRequested = adjustedSpendAmount < totalOutstandingAmount;
+
+            if (wasPartialPaymentRequested)
+            {
+                int remainingAmountOwedAfterSpend = totalOutstandingAmount - adjustedSpendAmount;
+
+                return Math.Max(0, totalTaxAmount - remainingAmountOwedAfterSpend);
+            }
+
+            return adjustedTaxAmount;
+        }
+
+        private static int CalculatedAdjustedExemptionAmount(
+            int totalOutstandingAmount,
+            int totalTaxAmount,
+            int totalExemptionAmount,
+            int adjustedSpendAmount)
+        {
+            int totalOutstandingAmountLessTax = totalOutstandingAmount - totalTaxAmount;
+
+            // don't consider exemption amounts < $0
+            // don't consider exemption amounts > spend amount
+            // don't consider exemption amounts > than subtotal
+            int adjustedExemptionAmount =
+                Math.Max(0, Math.Min(Math.Min(totalExemptionAmount, totalOutstandingAmountLessTax), adjustedSpendAmount));
+
+            bool wasPartialPaymentRequested = adjustedSpendAmount < totalOutstandingAmount;
+
+            if (wasPartialPaymentRequested)
+            {
+                // defer the exemption amount to the end
+                int totalOutstandingLessTaxAfterPayment = totalOutstandingAmountLessTax - adjustedSpendAmount;
+
+                adjustedExemptionAmount = Math.Max(0, adjustedExemptionAmount - totalOutstandingLessTaxAfterPayment);
+            }
+
+            return adjustedExemptionAmount;
+        }
+    }
+}
