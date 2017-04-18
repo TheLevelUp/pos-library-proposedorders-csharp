c62908fed2227ba4abe1d5dfd4326c99b390a985
Forest Balk <forest.balk@scvngr.com>
2017-04-18 14:25:12 -0400

    Adjusted variables to match calculator convention

--- ProposedOrderCalculator.Tests/CalculatorTests.cs
+++ ProposedOrderCalculator.Tests/CalculatorTests.cs
@@ -18,7 +18,7 @@ namespace LevelUp.Pos.ProposedOrderCalculator.Tests
             for (int i = 0; i < values.GetLength(0); i++)
             {
                 int totalOutstandingAmount = values[i, 0];
-                int totalOutstandingTaxAmount = values[i, 1];
+                int totalTaxAmount = values[i, 1];
                 int totalExemptionAmount = values[i, 2];
                 int spendAmount = values[i, 3];
 
@@ -31,7 +31,7 @@ namespace LevelUp.Pos.ProposedOrderCalculator.Tests
 
                 AdjustedCheckValues actualCheckValues = Calculator.CalculateAdjustedCheckValues(
                     totalOutstandingAmount,
-                    totalOutstandingTaxAmount,
+                    totalTaxAmount,
                     totalExemptionAmount,
                     spendAmount);
 
